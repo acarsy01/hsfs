ab5fcdf2c6ba86d2ba95a5f5d9897837aac82eb5
acarsy01 <acarsy01@gmail.com>
2020-07-21 15:05:37 +0300

    Deleted unnecessary files.

--- adapters/XMLHttpRequestAdapter.js
+++ adapters/XMLHttpRequestAdapter.js
@@ -5,7 +5,7 @@ module.exports = function ({
   url
 }) {
   let req = new XMLHttpRequest();
-  req.open(method, (new URL(url)));
+  req.open(`${method}`, (new URL(url)));
 
   for (let i = 0; i < Object.keys(headers).length; i++) {
     req.setRequestHeader(Object.keys(headers)[parseInt(i, 10)], Object.values(headers)[parseInt(i, 10)]);
--- test.ts
+++
@@ -1,9 +0,0 @@
-import * as util from "util";
-import * as hsfs from "./";
-import * as HTTP2Adapter from "./adapters/HTTP2Adapter";
-
-(async () => {
-  const data = hsfs("https://discord.com/api/users/@me").setAdapter(HTTP2Adapter);
-  process.stdout.write(util.inspect((await data.finalize()), true, null, true) + "\n");
-  process.stdout.write(hsfs.version);
-})();
